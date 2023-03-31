0ab2dedc11cf65afcf32e65ea9bbc20a4395d961
Timothy <todongoonditi@gmail.com>
2023-03-31 11:49:52 +0300

    last commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+const renderUserId = document.querySelector("#renderUserId")
+const catchError = document.querySelector("#error")
+
+  //Posting data to the server
+  function submitData(inputName, inputEmail){
+    return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify({
+        name: inputName,
+        email: inputEmail,
+      }),
+  })
+    .then(response => response.json())
+    .then(userDataRespose =>{
+        renderUserId.textContent = userDataRespose.id
+    })
+    .catch(error => {
+        catchError.textContent = error.message;
+        console.log(error.message);
+    });
+};
